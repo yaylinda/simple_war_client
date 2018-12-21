ff755fd68a7d79acab153ce2b7173272baa7d58d
yaylinda <lindazheng1993@gmail.com>
2018-12-20 22:12:56 -0600

    committing /ios and /android directories

---
+++ android/app/src/main/java/yay/linda/com/simplewarclient/MainActivity.java
@@ -0,0 +1,13 @@
+package yay.linda.com.simplewarclient;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
